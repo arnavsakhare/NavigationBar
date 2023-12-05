6742da90fbfccc53cd770d3b51d830fe79a02c5c
Arnav Sakhare <arnav.sakhare123@gmail.com>
2023-12-06 03:05:21 +0530

    Updated with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import {Route, Routes} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Pricing from './pages/Pricing';
 
 function App() {
+
+  
+
   return (
+   <>
     <Navbar />
+    <div className='cointainer'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </div>
+    
+   </>
   );
 }
 
